de04ef62636e8945b693533631c295a26a758c5a
Guilherme de Castro <guilycst@gmail.com>
2018-03-22 21:27:07 -0300

    First spring MVC features

---
+++ src/main/java/br/guilycst/controller/HomeController.java
@@ -0,0 +1,15 @@
+package br.guilycst.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+    @RequestMapping("/")
+    public String index(){
+        System.out.println("Entrando na home da CDC");
+        return "home";
+    }
+
+}
